bfb4720b099e97d31cb5ee2e99576a9a3e5e81b9
Amer Beganovic <a.began@gmail.com>
2019-03-11 21:23:28 +0100

    Refactored and added remaining methods

--- README.md
+++ README.md
@@ -10,31 +10,64 @@ A simple client for the [BunnyCDN API](https://bunnycdn.docs.apiary.io). **(WIP)
       apiKey: 'YOUR-API-KEY'
     })
 
-    client
-      .listPullzones()
-      .then(({ status, message, body }) => {
-        // status = 200
-        // message = 'OK'
-        // body = [{}, ...]
-      })
-      .catch(console.error)
+    client.pullzone.list().then(items => {
+      // items is an array of pullzones
+    }).catch(console.error)
 
 ## Methods
 
-See official [BunnyCDN documentation](https://bunnycdn.docs.apiary.io) for more info.
+**[client.purge(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipurge/post-purge-cache)**
 
-**listPullzones()**
+**[client.statistics(data)](https://bunnycdn.docs.apiary.io/#reference/0/apistatistics/get-statistics)**
 
-**createPullzone(data)**
+**[client.billing.list()](https://bunnycdn.docs.apiary.io/#reference/0/apibilling/get-billing-summary)**
 
-**getPullzone(id)**
+**[client.billing.applycode(data)](https://bunnycdn.docs.apiary.io/#reference/0/apibillingapplycode/get-apply-promo-code)**
 
-**updatePullzone(id, data)**
+### Pullzone
 
-**deletePullzone(id)**
+**[client.pullzone.list()](https://bunnycdn.docs.apiary.io/#reference/0/apipullzone/get-pull-zone-list)**
 
-**purgeCache(id)**
+**[client.pullzone.create(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzone/post-create-pull-zone)**
 
-**addPullzoneHostname(data)**
+**[client.pullzone.get(id)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneid/get-single-pull-zone)**
 
-**deletePullzoneHostname(data)**
+**[client.pullzone.update(id, data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneid/post-update-the-pull-zone)**
+
+**[client.pullzone.delete(id)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneid/delete-delete-the-pull-zone)**
+
+**[client.pullzone.purgeCache(id)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneidpurgecache/post-purge-pull-zone-cache)**
+
+**[client.pullzone.addHostname(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneaddhostname/post-add-custom-hostname)**
+
+**[client.pullzone.deleteHostname(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzonedeletehostname/delete-delete-custom-hostname)**
+
+**[client.pullzone.setForceSSL(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzonesetforcessl/post-set-the-force-ssl-setting)**
+
+**[client.pullzone.loadFreeCertificate(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneloadfreecertificate/get-load-free-certificate)**
+
+**[client.pullzone.addCertificate(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneaddcertificate/post-add-certificate)**
+
+**[client.pullzone.addBlockedIp(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneaddblockedip/post-add-blocked-ip)**
+
+**[client.pullzone.removeBlockedIp(data)](https://bunnycdn.docs.apiary.io/#reference/0/apipullzoneremoveblockedip/post-remove-blocked-ip)**
+
+### Storage zone
+
+**[client.storagezone.list()](https://bunnycdn.docs.apiary.io/#reference/0/apistoragezone/get-storage-zone-list)**
+
+**[client.storagezone.create(data)](https://bunnycdn.docs.apiary.io/#reference/0/apistoragezone/post-add-storage-zone)**
+
+**[client.storagezone.delete(id)](https://bunnycdn.docs.apiary.io/#reference/0/apistoragezoneid/delete-delete-the-storage-zone)**
+
+### Storage
+
+The storage API [documentation](https://bunnycdnstorage.docs.apiary.io) is separate from the main API docs.
+
+**[client.storage.download(path)](https://bunnycdnstorage.docs.apiary.io/#reference/0/storagezonenamepathfilename/get)**
+
+**[client.storage.upload(path)](https://bunnycdnstorage.docs.apiary.io/#reference/0/storagezonenamepathfilename/put)**
+
+**[client.storage.delete(path)](https://bunnycdnstorage.docs.apiary.io/#reference/0/storagezonenamepathfilename/delete)**
+
+**[client.storage.list(path)](https://bunnycdnstorage.docs.apiary.io/#reference/0/storagezonenamepath/get)**
--- index.js
+++ index.js
@@ -2,24 +2,23 @@ const got = require('got')
 const extend = require('extend')
 const pkg = require('./package.json')
 
-class BunnyCDN {
-  constructor (options = {}) {
-    if (!options.apiKey) {
-      throw new TypeError('API key is required.')
-    }
-
-    const defaults = {
-      baseUrl: 'https://bunnycdn.com/api'
-    }
-
-    this.options = extend(defaults, options)
+function bunnycdn (options = {}) {
+  let _options = {}
+  const _defaults = {
+    apiKey: null,
+    apiBaseUrl: 'https://bunnycdn.com/api',
+    storageBaseUrl: 'https://storage.bunnycdn.com'
   }
 
-  _caughtError (err) {
-    const error = {
-      status: 500,
-      message: 'Internal Server Error'
-    }
+  if (!options.apiKey) {
+    throw new TypeError('API key is required.')
+  }
+
+  _options = extend({}, _defaults, options)
+
+  function _throwError (err) {
+    const error = new Error('Internal Server Error')
+    error.status = 500
 
     if (err.statusCode) {
       error.status = err.statusCode
@@ -29,80 +28,183 @@ class BunnyCDN {
       error.message = err.statusMessage
     }
 
-    return error
+    if (err.body) {
+      error.body = err.body
+    }
+
+    throw error
   }
 
-  async _req (path = '', options = {}) {
+  async function _req (path = '', options = {}) {
     const defaults = {
+      baseUrl: _options.apiBaseUrl,
       method: 'GET',
       json: true,
-      baseUrl: this.options.baseUrl,
       headers: {
         'User-Agent': `bunnycdn-node-client/${pkg.version}`,
-        'AccessKey': this.options.apiKey
+        'AccessKey': _options.apiKey
       }
     }
 
     try {
       var res = await got(path, extend(defaults, options))
     } catch (err) {
-      return this._caughtError(err)
+      return _throwError(err)
     }
 
-    return {
-      status: 200,
-      message: 'OK',
-      body: res.body
+    return res.body
+  }
+
+  return {
+    purge (query) {
+      return _req('/purge', {
+        query,
+        method: 'POST'
+      })
+    },
+
+    statistics (query) {
+      return _req('/statistics', { query })
+    },
+
+    billing: {
+      list () {
+        return _req('/billing')
+      },
+
+      applycode (query) {
+        return _req('/billing/applycode', { query })
+      }
+    },
+
+    pullzone: {
+      list () {
+        return _req('/pullzone')
+      },
+
+      create (body) {
+        return _req('/pullzone', {
+          body,
+          method: 'POST'
+        })
+      },
+
+      get (id) {
+        return _req(`/pullzone/${id}`)
+      },
+
+      update (id, body) {
+        return _req(`/pullzone/${id}`, {
+          body,
+          method: 'POST'
+        })
+      },
+
+      delete (id) {
+        return _req(`/pullzone/${id}`, {
+          method: 'DELETE'
+        })
+      },
+
+      purgeCache (id) {
+        return _req(`/pullzone/${id}/purgeCache`, {
+          method: 'POST'
+        })
+      },
+
+      addHostname (body) {
+        return _req('/pullzone/addHostname', {
+          body,
+          method: 'POST'
+        })
+      },
+
+      deleteHostname (query) {
+        return _req('/pullzone/deleteHostname', {
+          query,
+          method: 'DELETE'
+        })
+      },
+
+      setForceSSL (body) {
+        return _req('/pullzone/setForceSSL', {
+          body,
+          method: 'POST'
+        })
+      },
+
+      loadFreeCertificate (query) {
+        return _req('/pullzone/addHostname', { query })
+      },
+
+      addCertificate (body) {
+        return _req('/pullzone/addCertificate', {
+          body,
+          method: 'POST'
+        })
+      },
+
+      addBlockedIp (body) {
+        return _req('/pullzone/addBlockedIp', {
+          body,
+          method: 'POST'
+        })
+      },
+
+      removeBlockedIp (body) {
+        return _req('/pullzone/removeBlockedIp', {
+          body,
+          method: 'POST'
+        })
+      }
+    },
+    storagezone: {
+      list () {
+        return _req('/storagezone')
+      },
+
+      create (body) {
+        return _req('/storagezone', {
+          body,
+          method: 'POST'
+        })
+      },
+
+      delete (id) {
+        return _req(`/storagezone/${id}`, {
+          method: 'DELETE'
+        })
+      }
+    },
+    storage: {
+      download (path) {
+        return _req(path, {
+          baseUrl: _options.storageBaseUrl
+        })
+      },
+
+      upload (path, body) {
+        return _req(path, {
+          body,
+          baseUrl: _options.storageBaseUrl,
+          method: 'PUT'
+        })
+      },
+
+      delete (path) {
+        return _req(path, {
+          baseUrl: _options.storageBaseUrl,
+          method: 'DELETE'
+        })
+      },
+
+      list (path) {
+        return _req(path, {
+          baseUrl: _options.storageBaseUrl
+        })
+      }
     }
-  }
-
-  listPullzones () {
-    return this._req('/pullzone')
-  }
-
-  createPullzone (body) {
-    return this._req('/pullzone', {
-      body,
-      method: 'POST'
-    })
-  }
-
-  getPullzone (id) {
-    return this._req(`/pullzone/${id}`)
-  }
-
-  updatePullzone (id, body) {
-    return this._req(`/pullzone/${id}`, {
-      body,
-      method: 'POST'
-    })
-  }
-
-  deletePullzone (id) {
-    return this._req(`/pullzone/${id}`, {
-      method: 'DELETE'
-    })
-  }
-
-  purgeCache (id) {
-    return this._req(`/pullzone/${id}/purgeCache`, {
-      method: 'POST'
-    })
-  }
-
-  addPullzoneHostname (body) {
-    return this._req(`/pullzone/addHostname`, {
-      body,
-      method: 'POST'
-    })
-  }
-
-  deletePullzoneHostname (query) {
-    return this._req(`/pullzone/addHostname`, {
-      query,
-      method: 'DELETE'
-    })
   }
 }
 
-module.exports = BunnyCDN
+module.exports = bunnycdn
